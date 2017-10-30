a8c4ecc4cf4826c8788c3df85ebf401f8399ec44
Robert Tumarkin <roberttumarkin@roberts-mac-pro.ad.unsw.edu.au>
2017-10-31 08:27:19 +1100

    Utility fixes

--- util/loop_download.fish
+++ util/loop_download.fish
@@ -1,7 +1,7 @@
 #!/usr/local/bin/fish
 for year in (seq 1993 2016)
   for qtr in (seq 1 4)
-    echo "edgar $year $qtr"
-    edgar $year $qtr
+    echo "edgar update $year $qtr"
+    edgar update $year $qtr
   end
 end
