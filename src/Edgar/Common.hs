97afc590343dfb40960ec20e4a2f548bc8dba28f
Rob Tumarkin <r.tumarkin@unsw.edu.au>
2020-06-15 19:49:32 +1000

    Bump resolver to 15.15. Reduced the overall number dependencies by switching to MicroLens Platform and Relude.

--- src/Edgar/Common.hs
+++ src/Edgar/Common.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module Edgar.Common
-  ( 
+  (
   -- * Types
     EdgarForm(..)
   , YearQtr
@@ -24,100 +22,103 @@ module Edgar.Common
 
   ) where
 
-import Data.Char (isDigit)
-import           ClassyPrelude as X
-import           Data.Time.Calendar
-import           Data.Time.Format
-import           Hasql.Connection
-import           Data.Csv
-import qualified Data.ByteString.Lazy.Char8 as L8
-import           Data.Functor.Contravariant
-import qualified Hasql.Decoders             as D
-import qualified Hasql.Encoders             as E
-import           Options.Applicative.Helper as X
-import qualified Options.Applicative        as Opt
-import GHC.Read
--- import Text.Parsec 
-import Prelude (read)
-import qualified Text.ParserCombinators.ReadP as RP
-import qualified Text.ParserCombinators.ReadPrec as RP
-import Text.ParserCombinators.Parsec hiding ((<|>))
+import           Data.Time.Calendar              as X (Day)
+import           Data.Time.Format                as X (defaultTimeLocale,
+                                                       formatTime, parseTimeM)
+import           Options.Applicative.Simple      as X
+import           Relude                          as X
+import           System.Directory                as X
+import           System.FilePath                 as X
 
-connectTo :: ByteString -> IO Connection
+import qualified Data.ByteString.Lazy.Char8      as L8
+import           Data.Char                       (isDigit)
+import           Data.Csv
+import           GHC.Read
+import           Hasql.Connection
+import qualified Hasql.Encoders                  as E
+import qualified Options.Applicative             as Opt
+import           Prelude                         (read)
+import           Text.ParserCombinators.Parsec   hiding ((<|>))
+import qualified Text.ParserCombinators.ReadP    as RP
+import qualified Text.ParserCombinators.ReadPrec as RP
+
+
+connectTo ∷ ByteString → IO Connection
 connectTo b = acquire b >>= \case
-  Left e  -> error "Unable to connect to database"
-  Right c -> return c
+    Left e  → error "Unable to connect to database"
+    Right c → return c
 
 
 data EdgarForm = EdgarForm
-  { cik         :: !Int64
-  , companyName :: !Text
-  , formType    :: !Text
-  , dateFiled   :: !Day
-  , filename    :: !Text
+  { cik         ∷ !Int64
+  , companyName ∷ !Text
+  , formType    ∷ !Text
+  , dateFiled   ∷ !Day
+  , filename    ∷ !Text
   } deriving (Generic, Show)
 
 instance FromRecord EdgarForm
 instance ToRecord   EdgarForm
 
 instance ToField Day where
-  toField = error "Day is not intended to be converted to a CSV field"
+    toField = error "Day is not intended to be converted to a CSV field"
 
 instance FromField Day where
-  -- parseField :: ByteString -> Parser Day
-  parseField = parseTimeM True defaultTimeLocale "%Y-%m-%d" . L8.unpack . L8.fromStrict
+    parseField = parseTimeM True defaultTimeLocale "%Y-%m-%d" . L8.unpack . L8.fromStrict
 
-encodeEdgarForm = contramap cik (E.value E.int8)
-           <> contramap companyName (E.value E.text)
-           <> contramap formType (E.value E.text)
-           <> contramap dateFiled (E.value E.date)
-           <> contramap filename (E.value E.text)
+encodeEdgarForm
+    = contramap cik (E.param $ E.nonNullable E.int8)
+   <> contramap companyName (E.param $ E.nonNullable E.text)
+   <> contramap formType (E.param $ E.nonNullable E.text)
+   <> contramap dateFiled (E.param $ E.nonNullable E.date)
+   <> contramap filename (E.param $ E.nonNullable E.text)
 
-textOption :: Opt.Mod Opt.OptionFields String -> Opt.Parser Text
-textOption ms = pack <$> Opt.strOption ms
+textOption ∷ Opt.Mod Opt.OptionFields String → Opt.Parser Text
+textOption ms = toText <$> Opt.strOption ms
 
 
 data YearQtr = YearQtr
-  { year :: !Int
-  , qtr  :: !Int
+  { year ∷ !Int
+  , qtr  ∷ !Int
   } deriving (Show, Eq, Ord)
 
-yearQtr :: Int -> Int -> YearQtr
-yearQtr y q = 
-    if | y < 1994      -> error "Edgar index starts in 1994"
-       | q < 1 || q > 4 -> error "Quarter must be between 1 and 4"
-       | otherwise     -> YearQtr y q
+yearQtr ∷ Int → Int → YearQtr
+yearQtr y q =
+    if | y < 1994      → error "Edgar index starts in 1994"
+       | q < 1 || q > 4 → error "Quarter must be between 1 and 4"
+       | otherwise     → YearQtr y q
 
 instance Read YearQtr where
-  readsPrec _ s = [(parseYq s, "")]
-  readPrec  = RP.lift readYqP
+    readsPrec _ s = [(parseYq s, "")]
+    readPrec  = RP.lift readYqP
 
-parseYq :: String -> YearQtr
-parseYq s = 
-  case Text.ParserCombinators.Parsec.runParser parseYq' () "YearQtr" s of
-    Left  e -> error $ show e
-    Right r -> r
+parseYq ∷ String → YearQtr
+parseYq s =
+    case Text.ParserCombinators.Parsec.runParser parseYq' () "YearQtr" s of
+      Left  e → error $ show e
+      Right r → r
 
 
-parseYq' :: Text.ParserCombinators.Parsec.Parser YearQtr
+parseYq' ∷ Text.ParserCombinators.Parsec.Parser YearQtr
 parseYq' = do
-  y <- many1 digit 
-  char 'q' <|> char 'Q'
-  q <- digit
-  return $ yearQtr (read y) (read $ q : "")
+    y <- many1 digit
+    char 'q' <|> char 'Q'
+    q <- digit
+    return $ yearQtr (read y) (read $ q : "")
 
 
-readYqP :: RP.ReadP YearQtr
+readYqP ∷ RP.ReadP YearQtr
 readYqP = do
-  y <- RP.count 4 $ RP.satisfy isDigit
-  _ <- RP.satisfy (\c -> c == 'q' || c == 'Q')
-  q <- RP.satisfy isDigit
-  _ <- RP.eof
-  return $ yearQtr (read y) (read $ q : "")
+    y <- RP.count 4 $ RP.satisfy isDigit
+    _ <- RP.satisfy (\c → c == 'q' || c == 'Q')
+    q <- RP.satisfy isDigit
+    _ <- RP.eof
+    return $ yearQtr (read y) (read $ q : "")
 
 instance Enum YearQtr where
-  fromEnum yq = year yq * 4 + (qtr yq - 1)
-  toEnum    i = YearQtr y m
-    where
-      m = mod i 4 + 1
-      y = quot i 4
+    fromEnum yq = year yq * 4 + (qtr yq - 1)
+    toEnum    i = YearQtr y m
+      where
+        m = mod i 4 + 1
+        y = quot i 4
+
--- src/Edgar/Concurrent.hs
+++ src/Edgar/Concurrent.hs
@@ -2,16 +2,17 @@ module Edgar.Concurrent
   ( runConcurrent
   ) where
 
+import           Control.Concurrent           (forkIO)
+import           GHC.Conc                     (threadDelay)
+import           GHC.Conc.Sync                (ThreadId, modifyMVar_)
+import           Relude
+import           System.Console.AsciiProgress
 
-import ClassyPrelude
-import Control.Concurrent (forkIO)
-import System.Console.AsciiProgress
 
-
-runConcurrent :: Int             -- | Number of processed to run
-              -> (a -> IO ()) -- | Form processing function
-              -> [a]         -- | List of forms to process
-              -> IO ()
+runConcurrent ∷ Int         -- | Number of processed to run
+              → (a → IO ()) -- | Form processing function
+              → [a]         -- | List of forms to process
+              → IO ()
 runConcurrent nConcurrent f forms = do
     queue <- newMVar forms
     dc    <- newMVar 0
@@ -19,49 +20,56 @@ runConcurrent nConcurrent f forms = do
 
     renderProgressBarUntilComplete (length forms) dc
 
-spawnThreads :: Int -> (a -> IO ()) -> (Queue a, ProgressCounter) -> IO [ThreadId]
+--------------------------------------------------------------------------------
+-- Thread control                                                             --
+--------------------------------------------------------------------------------
+spawnThreads ∷ Int → (a → IO ()) → (Queue a, ProgressCounter) → IO [ThreadId]
 spawnThreads nConcurrent f qc =
     replicateM nConcurrent $ forkIO (singleThread f qc)
 
-singleThread :: (a -> IO ()) -- | Form processing function
-             -> (Queue a, ProgressCounter) 
-             -> IO ()
+singleThread ∷ (a → IO ()) -- | Form processing function
+             → (Queue a, ProgressCounter)
+             → IO ()
 singleThread f (q, dc) =
-  nextForm q >>= \case
-    Nothing -> return ()
-    Just nf -> f nf >> incrementCounter dc >> singleThread f (q, dc)
+    nextForm q >>= \case
+      Nothing → return ()
+      Just nf → f nf >> incrementCounter dc >> singleThread f (q, dc)
 
--- -- Asynchronous queue and counter
+--------------------------------------------------------------------------------
+-- Asynchronous queue and counter                                             --
+--------------------------------------------------------------------------------
 type Queue a         = MVar [a]
 type ProgressCounter = MVar Integer -- A counter for the number downloaded and not yet included in progress bar
 
 
-nextForm :: Queue a -> IO (Maybe a)
+nextForm ∷ Queue a → IO (Maybe a)
 nextForm q = do
-  queue <- takeMVar q
-  case fromNullable queue of
-    Nothing -> putMVar q queue >> return Nothing
-    Just x  -> putMVar q (tail x) >> return (Just $ head x)
+    queue <- takeMVar q
+    case queue of
+      []     → putMVar q queue >> return Nothing
+      (x:xs) → putMVar q xs >> return (Just x)
 
-incrementCounter :: ProgressCounter -> IO ()
+incrementCounter ∷ ProgressCounter → IO ()
 incrementCounter dc = modifyMVar_ dc (return . (+1))
 
-renderProgressBarUntilComplete :: Int -> ProgressCounter -> IO ()
-renderProgressBarUntilComplete nForms dc = displayConsoleRegions $ do
-  pg <- newProgressBar def { pgWidth       = 100
-                          , pgOnCompletion = Just "Done: :percent"
-                          , pgTotal        = fromIntegral nForms
-                          }
-  loopProgressBar dc pg
 
-loopProgressBar :: ProgressCounter -> ProgressBar -> IO ()
-loopProgressBar dc pg =
-  unlessM (isComplete pg) $ do
-    n <- takeMVar dc
-    putMVar dc 0
-    tickNI pg n
-    threadDelay $ 100 * 1000
+--------------------------------------------------------------------------------
+-- Rendering                                                                  --
+--------------------------------------------------------------------------------
+renderProgressBarUntilComplete ∷ Int → ProgressCounter → IO ()
+renderProgressBarUntilComplete nForms dc = displayConsoleRegions $ do
+    pg <- newProgressBar def { pgWidth       = 100
+                            , pgOnCompletion = Just "Done: :percent"
+                            , pgTotal        = fromIntegral nForms
+                            }
     loopProgressBar dc pg
 
-
+loopProgressBar ∷ ProgressCounter → ProgressBar → IO ()
+loopProgressBar dc pg =
+    unlessM (isComplete pg) $ do
+      n <- takeMVar dc
+      putMVar dc 0
+      tickNI pg n
+      threadDelay $ 100 * 1000
+      loopProgressBar dc pg
 
